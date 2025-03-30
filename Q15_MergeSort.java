52abb597073b3944fba2fdcfead542874c5df507
deepakkroxford <deepakkr.oxford@gmail.com>
2025-03-30 09:44:29 +0530

    adding the quick sort and merge sort

---
+++ Q15_MergeSort.java
@@ -0,0 +1,53 @@
+public class Q15_MergeSort {
+
+    public static void merge(int arr[], int low, int mid, int high) {
+        int left = low;
+        int right = mid+1;
+        int temp[] = new int[high - low + 1];
+        int index = 0;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+
+        while (right <= high) {
+            temp[index++] = arr[right++];
+        }
+
+        System.arraycopy(temp, 0, arr, low, temp.length);
+    }
+
+    public static void mergeSort(int arr[], int low, int high) {
+        /*
+         * base case
+         */
+        if (low >= high) {
+            return;
+        }
+        int mid = low + (high - low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 3, 1, 2, 4, 1, 5, 2, 6, 4 };
+        mergeSort(arr, 0, arr.length-1);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    System.out.println("Time Complexity: O(n log n) - The array is repeatedly divided into halves (log n) and then merged (n)");
+    System.out.println("Space Complexity: O(n) - Additional temporary array is used during merging");
+
+    }
+}
---
+++ Q16_QuickSort.java
@@ -0,0 +1,47 @@
+public class Q16_QuickSort {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+
+            while (arr[i] <= pivot && i < high ) {
+                i++;
+            }
+            while (arr[j] > pivot && j > low) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int partionIndex = partition(arr, low, high);
+            quickSort(arr, low, partionIndex - 1);
+            quickSort(arr, partionIndex + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 1, 2, 4, 1, 5, 2, 6, 4 };
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+        System.out.println(
+                "Time Complexity: O(n log n) - Average case, O(n^2) in worst case (when pivot is always smallest or largest element)");
+        System.out.println("Space Complexity: O(log n) - Due to recursion stack");
+    }
+}
